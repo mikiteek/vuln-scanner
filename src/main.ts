01baf32908ca8237489c20b2900c05f23c4cf1f0
Sergii Mykyteiek <mikiteek@gmail.com>
2026-02-01 13:27:49 +0100

    Add memory metrics tracking in interval

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filter';
 
+const trackMemoryMetrics = (logger: Logger) =>
+  setInterval(() => {
+    const memory = process.memoryUsage();
+    const totalProcessMemory = `${(memory.rss / 1024 / 1024).toFixed(2)} MB`;
+    const heapUsed = `${(memory.heapUsed / 1024 / 1024).toFixed(2)} MB`;
+
+    logger.log(`Total process memory: ${totalProcessMemory}`);
+    logger.log(`Heap used: ${heapUsed}`);
+  }, 1000);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -12,6 +22,11 @@ async function bootstrap() {
   const logger = app.get(Logger);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT', 3000);
+
+  const metricsEnabled = configService.get<string>('METRICS_ENABLED', '0');
+  if (metricsEnabled === '1') {
+    trackMemoryMetrics(logger);
+  }
 
   await app.listen(port, () => {
     logger.log(`Server is running on port ${port} !!!!!!!!!!!!`);
